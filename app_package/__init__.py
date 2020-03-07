62b04e0eedadc1402562a4532f7d222c86f9a90a
AnuJohnson169 <anu1697pj@gmail.com>
2020-03-07 10:43:06 +0530

    commit by anu

--- app_package/__init__.py
+++ app_package/__init__.py
@@ -2,7 +2,6 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_pymongo import PyMongo
-from flask_login import LoginManager
 from app_package.config import Config
 
 app=Flask(__name__)
@@ -11,7 +10,7 @@ db=SQLAlchemy(app)
 migrate=Migrate(app,db)
 mongo=PyMongo(app)
 
-login_manager=LoginManager(app)
-login_manager.login_view="index"
+#login_manager=LoginManager(app)
+#login_manager.login_view="index"
 
-from app_package import routes, models,errors
+from app_package import resource_routes
